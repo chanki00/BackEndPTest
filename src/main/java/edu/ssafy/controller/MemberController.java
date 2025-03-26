5b0a049319da4f7887381f6b7f5f1e4bc1b250e3
chanki00 <rlacksrl00@naver.com>
2025-03-26 17:49:04 +0900

    add

--- src/main/java/edu/ssafy/controller/MemberController.java
+++ src/main/java/edu/ssafy/controller/MemberController.java
@@ -1,8 +1,16 @@
 package edu.ssafy.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 
 @WebServlet("/member")
-public class MemberController {
-
+public class MemberController extends HttpServlet {
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+	
+	
 }
--- src/main/java/edu/ssafy/service/MemberService.java
+++ src/main/java/edu/ssafy/service/MemberService.java
@@ -1,5 +1,11 @@
 package edu.ssafy.service;
 
+/**
+ * CRUD - insert / select / update / delete
+ */
+
 public interface MemberService {
+	
+	public int insertMember() throws Exception;
 
 }
